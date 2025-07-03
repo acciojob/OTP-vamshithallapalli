dbbe3a9b969f1ca0896eecd558c1fcd3affd73ae
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-07-03 13:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    if (/^\d$/.test(value)) {
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    } else {
+      e.target.value = "";
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = "";
+      }
+    }
+  });
+});
